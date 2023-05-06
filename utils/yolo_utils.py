0a01b9fb65bed02ec77a48461845ff88f69331b5
Jawher Ben Abdallah <j.benabdallah@avidea.tn>
2023-05-06 16:41:49 +0100

    Added simple inference function for YOLO.

--- utils/yolo_utils.py
+++ utils/yolo_utils.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 
 
-def run_object_detection():
-    ...
+def run_object_detection(weights_path, image):
+    model = YOLO(weights_path)
+    results = model.predict(image)
+    return results
