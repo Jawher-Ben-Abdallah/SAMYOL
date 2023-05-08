e82955d657544b81aaaa02cdebc6ab28a3f2bd85
Rim Sleimi <rim.sleimi@etudiant-fst.utm.tn>
2023-05-08 23:25:06 +0200

    packaging yolo_anything

--- predict.py
+++ predict.py
@@ -49,4 +49,3 @@ if __name__ == "__main__":
         args.sam_checkpoint,
         args.device
     )
-    
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+  name="YOLO_Anything",
+  version="1.0.0",
+  description="Combines YOLOv8 and SAM",
+  package_dir={"": "yolo_anything"},
+  packages=find_packages(where="yolo_anything"),
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Jawher-Ben-Abdallah/YOLO-SAM",
+  author="Jawher Ben Abdallah - Rim Sleimi",
+  author_email="jawher.b.abdallah@gmail.com - sleimi.rim1996@gmail.com",
+  license='Apache License 2.0',
+  classifiers=[
+      "License :: OSI Approved :: Apache 2.0 License",
+      "Programming Language :: Python :: 3.10",
+    ],
+    keywords="YOLOv8, SAM, Object_Detection, Segmentation",
+    install_requires=[
+        "matplotlib>=3.2.2",
+        "numpy>=1.21.6",
+        "opencv-python>=4.6.0",
+        "Pillow>=7.1.2",
+        "PyYAML>=5.3.1",
+        "requests>=2.23.0",
+        "scipy>=1.4.1",
+        "torch>=1.7.0",
+        "torchvision>=0.8.1",
+        "tqdm>=4.64.0",
+        "ultralytics",
+    ],
+    extras_require={"dev" : "twine>=4.0.2"},
+    python_requires=">=3.10",
+)
