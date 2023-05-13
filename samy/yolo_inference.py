67e251bbd3f74a9ba4131144d23ae5747772fa33
Jawher Ben Abdallah <j.benabdallah@avidea.tn>
2023-05-13 23:23:42 +0100

    Added inference for yolo v6 (onnx).

--- samy/yolo_inference.py
+++ samy/yolo_inference.py
@@ -1,3 +1,4 @@
+import onnxruntime as ort
 from super_gradients.training import models
 
 
@@ -5,8 +6,13 @@ class YOLOInference():
     def __init__(self) -> None:
         pass
 
-    def run_yolo_6_inference():
-        pass
+    def run_yolo_6_inference(inputs, model_path):
+        providers = ["CPUExecutionProvider"]
+        session = ort.InferenceSession(model_path, providers=providers)
+        outname = [i.name for i in session.get_outputs()]
+        inname = [i.name for i in session.get_inputs()]
+        detections = session.run(outname,{inname[0]:inputs})
+        return detections
 
     def run_yolo_7_inference():
         pass
@@ -20,4 +26,5 @@ class YOLOInference():
             num_classes=len(classes),
             checkpoint_path=model_path
         )
-        return model.predict(inputs)
+        detections = model.predict(inputs)
+        return detections
