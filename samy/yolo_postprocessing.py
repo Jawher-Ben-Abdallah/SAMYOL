906e54bca18dd058d5330e521e003dc6659d2fca
Jawher Ben Abdallah <j.benabdallah@avidea.tn>
2023-05-13 20:34:50 +0100

    Prepared template file for diffrent yolo versions postprocessing

---
+++ samy/yolo_postprocessing.py
@@ -0,0 +1,15 @@
+class YOLOPostProcessing():
+    def __init__(self) -> None:
+        print("Did a bunch of stuff")
+
+    def get_yolo_6_postprocessing():
+        print("Fetching yolo 6 postprocessing")
+    
+    def get_yolo_7_postprocessing():
+        print("Fetching yolo 7 postprocessing")
+    
+    def get_yolo_8_postprocessing():
+        print("Fetching yolo 8 postprocessing")
+
+    def get_yolo_nas_postprocessing():
+        print("Fetching yolo nas postprocessing")
