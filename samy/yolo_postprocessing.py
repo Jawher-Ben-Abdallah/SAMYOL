49c44bd67ce8646b4b8c806c0fb820e80f322d76
Jawher Ben Abdallah <j.benabdallah@avidea.tn>
2023-05-13 22:14:44 +0100

    Added NAS postprocessing func

--- samy/yolo_postprocessing.py
+++ samy/yolo_postprocessing.py
@@ -7,9 +7,23 @@ class YOLOPostProcessing():
     
     def get_yolo_7_postprocessing():
         print("Fetching yolo 7 postprocessing")
-    
+
     def get_yolo_8_postprocessing():
         print("Fetching yolo 8 postprocessing")
 
-    def get_yolo_nas_postprocessing():
-        print("Fetching yolo nas postprocessing")
+    def get_yolo_nas_postprocessing(detections):
+        object_detection_predictions = []
+        for i, detection in enumerate(detections):
+            class_names = detection.class_names
+            labels = detection.predction.labels
+            confidence = detection.prediction.confidence
+            bboxes = detection.prediction.bboxes_xyxy
+
+            for label, conf, bbox in zip(labels, confidence, bboxes):
+                object_detection_predictions.append({
+                    'image_id': i,
+                    'class_id': class_names[int(label)],
+                    'confidence': conf,
+                    'bbox': bbox
+                })
+        return object_detection_predictions
