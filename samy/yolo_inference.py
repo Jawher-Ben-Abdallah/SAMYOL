b89df059e2444f12c74e698b539ed91286c71e52
Jawher Ben Abdallah <j.benabdallah@avidea.tn>
2023-05-13 22:21:02 +0100

    Added template for yolo inference.

---
+++ samy/yolo_inference.py
@@ -0,0 +1,15 @@
+class YOLOInference():
+    def __init__(self) -> None:
+        pass
+
+    def run_yolo_6_inference():
+        pass
+
+    def run_yolo_7_inference():
+        pass
+
+    def run_yolo_8_inference():
+        pass
+
+    def run_yolo_nas_inference():
+        pass
