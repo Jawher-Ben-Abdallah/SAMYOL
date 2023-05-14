f9cb13de7d38a060e2b6fae1b3eb43d969b88cf6
Jawher Ben Abdallah <j.benabdallah@avidea.tn>
2023-05-14 22:24:43 +0100

    Fix minor merge conflicts.

--- samy/yolo_inference.py
+++ samy/yolo_inference.py
@@ -1,5 +1,5 @@
 import subprocess
-from utils import letterbox, generic_ort_inference
+from .utils import generic_ort_inference
 import onnxruntime as ort
 from super_gradients.training import models
 
@@ -7,19 +7,19 @@ from super_gradients.training import models
 class YOLOInference():
 
     @staticmethod
-    def run_yolo_6_inference(model_path, inputs):
+    def get_yolo_6_inference(model_path, inputs):
         detections = generic_ort_inference(model_path, inputs[0])
         resize_data, origin_RGB = inputs[1:]
         return detections, resize_data, origin_RGB
 
-    def run_yolo_7_inference(self, weights_path, cuda, inp, outname):
+    def get_yolo_7_inference(self, weights_path, cuda, inp, outname):
         providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
         session = ort.InferenceSession(weights_path, providers=providers)
         return session.run(outname, inp)[0]
         
 
 
-    def run_yolo_8_inference(self, weights_path, image):
+    def get_yolo_8_inference(self, weights_path, image):
 
         try:
             from ultralytics import YOLO
@@ -38,7 +38,7 @@ class YOLOInference():
         return detections
 
     
-    def run_yolo_nas_inference(inputs, model_type, model_path, classes):
+    def get_yolo_nas_inference(inputs, model_type, model_path, classes):
         model = models.get(
             model_type,
             num_classes=len(classes),
