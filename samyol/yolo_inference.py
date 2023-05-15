f7ae8270dc0333ad619d0b63c302acb368512c22
Rim Sleimi <rim.sleimi@etudiant-fst.utm.tn>
2023-05-15 17:40:09 +0200

    refactor code

--- samyol/yolo_inference.py
+++ samyol/yolo_inference.py
@@ -41,7 +41,7 @@ class YOLOInference():
     
     @staticmethod
     def get_yolo_nas_inference(inputs, model_type, model_path, classes):
-        
+        # TODO: Change this block to automatically download the package in case it is not available
         try:
             from super_gradients.training import models
         except ImportError:
